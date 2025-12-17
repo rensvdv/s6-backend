271c2154720e681f1e2bc0f2c17da6f9088bee6f
rensv <491926@student.fontys.nl>
2025-12-17 14:20:49 +0100

    unused import verwijderd

--- api-gateway/src/main/java/com/rensv/api_gateway/CorsConfig.java
+++ api-gateway/src/main/java/com/rensv/api_gateway/CorsConfig.java
@@ -3,7 +3,6 @@ package com.rensv.api_gateway;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 
@@ -14,8 +13,8 @@ public class CorsConfig {
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration corsConfig = new CorsConfiguration();
 
-        corsConfig.addAllowedOrigin("http://localhost:5173");  // Vue dev server
-        corsConfig.addAllowedOrigin("http://localhost:8080");  // Indien nodig
+        corsConfig.addAllowedOrigin("http://localhost:5173");
+        corsConfig.addAllowedOrigin("http://localhost:8080");
         corsConfig.addAllowedMethod("*");
         corsConfig.addAllowedHeader("*");
         corsConfig.setAllowCredentials(true);
